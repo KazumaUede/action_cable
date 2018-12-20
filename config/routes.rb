e80cf5b2bae0b73d1396c7d3728ce53d878e3c69
KazumaUede <katsuodashi794@gmail.com>
2018-12-20 15:29:13 +0900

    Rails g controller users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'rooms/show'
 
   # Serve websocket cable requests in-process
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217051818) do
+ActiveRecord::Schema.define(version: 20181220062839) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
