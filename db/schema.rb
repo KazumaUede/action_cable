3089fcfb30d6ec89f1ea26ff1dddef511c09fbf2
KazumaUede <katsuodashi794@gmail.com>
2018-12-20 15:48:21 +0900

    Test email and delete users.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220062839) do
+ActiveRecord::Schema.define(version: 20181220064517) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20181220062839) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
