4a3d62a84920c1cb5910a1f1da6263af9db6c80f
KazumaUede <katsuodashi794@gmail.com>
2018-12-20 15:49:47 +0900

    rails generate migration add_password_digest_to_users password_digest:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220064517) do
+ActiveRecord::Schema.define(version: 20181220064859) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20181220064517) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
